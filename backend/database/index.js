0da2a66df6e84f03b34d9fb25cda469f6888c718
lovegreenearth <greenearth1019@gmail.com>
2023-08-10 05:48:55 -0700

    DB update again

--- backend/database/index.js
+++ backend/database/index.js
@@ -3,26 +3,41 @@ require('dotenv').config();
 
 /* This is creating a connection to the database. */
 
-var config_db = {
-      // Host name for database connection:
-      host: 'db-mysql-nyc1-71256-do-user-14497985-0.b.db.ondigitalocean.com',
-      // Port number for database connection:
+// var config_db = {
+//       // Host name for database connection:
+//       host: 'db-mysql-nyc1-71256-do-user-14497985-0.b.db.ondigitalocean.com',
+//       // Port number for database connection:
+//       port: 25060,
+//       // Database user:
+//       user: 'doadmin',
+//       // Password for the above database user:
+//       password: 'AVNS_9O3tKO6NcswIT5v-eMK',
+//       // Database name:
+//       database: 'defaultdb'
+// };
+
+// var db = mysql.createPool(config_db); // or mysql.createConnection(config_db);
+
+// /* This is creating a connection to the database. */
+// db.getConnection((err, connection) => {
+//       if (err) throw err;
+//       console.log('🗃  DB connected successful: ' + connection.threadId);
+//       connection.release();
+// });
+
+// module.exports = db;
+
+var con = mysql.createConnection({
+      host: "db-mysql-nyc1-71256-do-user-14497985-0.b.db.ondigitalocean.com",
+      user: "doadmin",
+      password: "AVNS_9O3tKO6NcswIT5v-eMK",
       port: 25060,
-      // Database user:
-      user: 'doadmin',
-      // Password for the above database user:
-      password: 'AVNS_9O3tKO6NcswIT5v-eMK',
-      // Database name:
       database: 'defaultdb'
-};
-
-var db = mysql.createPool(config_db); // or mysql.createConnection(config_db);
-
-/* This is creating a connection to the database. */
-db.getConnection((err, connection) => {
-      if (err) throw err;
-      console.log('🗃  DB connected successful: ' + connection.threadId);
-      connection.release();
 });
 
-module.exports = db;
+con.connect(function (err) {
+      if (err) throw err;
+      console.log("Connected!");
+});
+
+module.exports = con;
