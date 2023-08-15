a2aafe302d22178da36a115a2e60ab437cb7db3c
lovegreenearth <greenearth1019@gmail.com>
2023-08-15 07:36:05 -0700

    live config

--- backend/database/index.js
+++ backend/database/index.js
@@ -3,37 +3,37 @@ require('dotenv').config();
 
 /* This is creating a connection to the database. */
 
-// var config_db = {
-//       // Host name for database connection:
-//       host: 'db-mysql-nyc1-71256-do-user-14497985-0.b.db.ondigitalocean.com',
-//       // Port number for database connection:
-//       port: 25060,
-//       // Database user:
-//       user: 'doadmin',
-//       // Password for the above database user:
-//       password: 'AVNS_9O3tKO6NcswIT5v-eMK',
-//       // Database name:
-//       database: 'defaultdb',
-//       waitForConnections: true,
-//       connectionLimit: 10,
-//       queueLimit: 0
-// };
-
 var config_db = {
       // Host name for database connection:
-      host: 'localhost',
+      host: 'db-mysql-nyc1-71256-do-user-14497985-0.b.db.ondigitalocean.com',
       // Port number for database connection:
-      port: 3306,
+      port: 25060,
       // Database user:
-      user: 'root',
+      user: 'doadmin',
       // Password for the above database user:
-      password: '',
+      password: 'AVNS_9O3tKO6NcswIT5v-eMK',
       // Database name:
-      database: 'excel-price',
+      database: 'defaultdb',
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0
 };
+
+// var config_db = {
+//       // Host name for database connection:
+//       host: 'localhost',
+//       // Port number for database connection:
+//       port: 3306,
+//       // Database user:
+//       user: 'root',
+//       // Password for the above database user:
+//       password: '',
+//       // Database name:
+//       database: 'excel-price',
+//       waitForConnections: true,
+//       connectionLimit: 10,
+//       queueLimit: 0
+// };
 
 var db = mysql.createPool(config_db); // or mysql.createConnection(config_db);
 
